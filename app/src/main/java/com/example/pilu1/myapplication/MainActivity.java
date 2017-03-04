b1cbe3b725e50b1cbf4370cc20765f437a3361d1
PeDua <thienthien.1551996@gmail.com>
2017-03-04 15:42:49 +0700

    commit student.java

--- app/src/main/java/com/example/pilu1/myapplication/MainActivity.java
+++ app/src/main/java/com/example/pilu1/myapplication/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.pilu1.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);//bỏ thanh title bar
         setContentView(R.layout.activity_main);
     }
 }
